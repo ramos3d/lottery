ecf51e2459d0a8eff4d49caf4f8189b973178d52
Marcelo Soares <marceloxramos3d@gmail.com>
2019-07-26 15:24:17 -0300

    Shaping the layout with Bootstrap4

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,3 +1,13 @@
-function addNumber(){
-    console.alert('Ok');
+
+function addNumber(number){
+    var btn = "btn"+number;
+    document.getElementById(btn).disabled = true;
+
+    document.getElementById("collection").value += ' ' + number;
+    console.log(number);
 }
+//console.log("ready!");
+//document.write(pad2(1));
+// $(document).ready(function(){
+//     console.log("ready!");
+// });
--- app/includes/header.php
+++ app/includes/header.php
@@ -4,4 +4,7 @@
     <meta charset="utf-8">
     <title>ArtificialBrain | Lottery</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
+    <link rel="shortcut icon" href="favicon.png">
 </head>
