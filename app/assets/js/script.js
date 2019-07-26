5bb62fe1aa530d6c6c9871c9de33b9eaed4e0e3b
Marcelo Soares <marceloxramos3d@gmail.com>
2019-07-26 15:31:08 -0300

    Cleaning up unnecessary comments

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -2,12 +2,6 @@
 function addNumber(number){
     var btn = "btn"+number;
     document.getElementById(btn).disabled = true;
-
     document.getElementById("collection").value += ' ' + number;
     console.log(number);
 }
-//console.log("ready!");
-//document.write(pad2(1));
-// $(document).ready(function(){
-//     console.log("ready!");
-// });
