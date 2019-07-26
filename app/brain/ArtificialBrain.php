8762ff2eb342e59cbafb2a20b513cdc22cc9aea6
Marcelo Soares <marceloxramos3d@gmail.com>
2019-07-26 10:28:30 -0300

    Structuring the directories

---
+++ app/brain/ArtificialBrain.php
@@ -0,0 +1,52 @@
+<?php
+namespace brain;
+
+/**
+*
+*/
+class ArtificialBrain
+{
+    private $data;
+    /**
+    *
+    */
+    public function addicted(){
+
+    }
+
+    /**
+    *
+    */
+    public function predictive(){
+
+    }
+
+    /**
+    *
+    */
+    public function prefixed(){
+
+    }
+
+    /**
+    *
+    */
+    public function aleatory(){
+
+    }
+
+    /**
+    *
+    */
+    public function byExclusion(){
+
+    }
+
+    /**
+    *
+    */
+    public function getFullResults(){
+
+    }
+
+}
